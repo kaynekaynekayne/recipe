f5678992bc403e806d9222864d86c19c586e8f26
myplacecoffee <74812757+myplacecoffee@users.noreply.github.com>
2022-07-10 17:01:24 +0900

    make useParams in cuisine component

--- src/pages/cuisine.jsx
+++ src/pages/cuisine.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import { Link, useParams } from 'react-router-dom';
 
 const Cuisine = () => {
 
+    const [cuisine,setCuisine]=useState([]);
+    let params=useParams();
+
+    const getCuisine=async(name)=>{
+        const response=await fetch(
+            `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisines=${name}`);
+        const recipe=await response.json();
+        setCuisine(recipe.results);
+    }
+
+    console.log(cuisine);
+
+    useEffect(()=>{
+        getCuisine(params.type);
+    },[params.type]);
+
+
     return(
-        <div>cuisine</div>
+        <div>
+            <h1>Cuisine</h1>
+        </div>
     )
 };
+
+
 
 export default Cuisine;
